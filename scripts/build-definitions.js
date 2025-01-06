f5215b85ebb7f58acd7c7488c9806434d099ae0a
futurGH <me@futuristick.ca>
2025-01-05 20:01:11 -0500

    Modify build script to allow hacking in custom keyboard definitions

--- scripts/build-definitions.js
+++ scripts/build-definitions.js
@@ -1,26 +1,48 @@
 const fs = require('fs');
-const { exec } = require('child_process');
+const { keyboardDefinitionV2ToVIADefinitionV2, keyboardDefinitionV3ToVIADefinitionV3 } = require('@the-via/reader');
+const { createHmac } = require('node:crypto');
 
 function buildDefinitions() {
-  if (fs.existsSync('./public/definitions')) {
-    return;
+  if (fs.existsSync('./public/definitions/v2')) {
+    fs.rmdirSync('./public/definitions/v2', { recursive: true });
+  }
+  if (fs.existsSync('./public/definitions/v3')) {
+    fs.rmdirSync('./public/definitions/v3', { recursive: true });
   }
 
-  console.log('Definitions not found. Building...');
+  console.log('Building...');
 
-  exec('via-keyboards public/definitions', (error, stdout, stderr) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-     
-    if (stderr) {
-      console.error('stderr');
-      return;
-    }
+  const vpids = { v2: [], v3: [] };
 
-    console.log(stdout);
-  });
+  for (const version of ['v2', 'v3']) {
+    if (!fs.existsSync(`./public/keyboards/${version}`)) continue;
+    fs.mkdirSync(`./public/definitions/${version}`, { recursive: true });
+    for (const filename of fs.readdirSync(`./public/keyboards/${version}`)) {
+      if (!filename.endsWith('.json')) continue;
+      const keyboardDefinition = require(`../public/keyboards/${version}/${filename}`);
+      const viaDefinition = version === 'v2'
+        ? keyboardDefinitionV2ToVIADefinitionV2(keyboardDefinition)
+        : keyboardDefinitionV3ToVIADefinitionV3(keyboardDefinition);
+      fs.writeFileSync(`./public/definitions/${version}/${filename}`, JSON.stringify(viaDefinition, null, 2));
+      vpids[version].push(viaDefinition.vendorProductId);
+    }
+  }
+
+  const supportedKbs = require('../public/definitions/supported_kbs.json');
+  supportedKbs.vendorProductIds = vpids;
+  fs.writeFileSync('./public/definitions/supported_kbs.json', JSON.stringify(supportedKbs, null, 2));
+
+  const hash = hashJSON(supportedKbs);
+
+  fs.writeFileSync('./public/definitions/hash.json', hash);
 }
+
+const hashJSON = (json) => hashText(JSON.stringify(json));
+
+const hashText = (text) => {
+  const hmac = createHmac('sha256', 'a secret');
+  hmac.update(text);
+  return hmac.digest('hex');
+};
 
 buildDefinitions();
