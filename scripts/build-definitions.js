a9a410426278c304ab66c136634bbcc594da8521
futurGH <me@futuristick.ca>
2025-02-17 11:31:40 -0500

    Fix Sprout json

--- scripts/build-definitions.js
+++ scripts/build-definitions.js
@@ -1,36 +1,48 @@
 const fs = require('fs');
-const { keyboardDefinitionV2ToVIADefinitionV2, keyboardDefinitionV3ToVIADefinitionV3 } = require('@the-via/reader');
-const { createHmac } = require('node:crypto');
+const {
+  keyboardDefinitionV2ToVIADefinitionV2,
+  keyboardDefinitionV3ToVIADefinitionV3,
+} = require('@the-via/reader');
+const {createHmac} = require('node:crypto');
 
 function buildDefinitions() {
   if (fs.existsSync('./public/definitions/v2')) {
-    fs.rmdirSync('./public/definitions/v2', { recursive: true });
+    fs.rmSync('./public/definitions/v2', {recursive: true});
   }
   if (fs.existsSync('./public/definitions/v3')) {
-    fs.rmdirSync('./public/definitions/v3', { recursive: true });
+    fs.rmSync('./public/definitions/v3', {recursive: true});
   }
 
   console.log('Building...');
 
-  const vpids = { v2: [], v3: [] };
+  const vpids = {v2: [], v3: []};
 
   for (const version of ['v2', 'v3']) {
     if (!fs.existsSync(`./public/keyboards/${version}`)) continue;
-    fs.mkdirSync(`./public/definitions/${version}`, { recursive: true });
+    fs.mkdirSync(`./public/definitions/${version}`, {recursive: true});
     for (const filename of fs.readdirSync(`./public/keyboards/${version}`)) {
       if (!filename.endsWith('.json')) continue;
-      const keyboardDefinition = require(`../public/keyboards/${version}/${filename}`);
-      const viaDefinition = version === 'v2'
-        ? keyboardDefinitionV2ToVIADefinitionV2(keyboardDefinition)
-        : keyboardDefinitionV3ToVIADefinitionV3(keyboardDefinition);
-      fs.writeFileSync(`./public/definitions/${version}/${filename}`, JSON.stringify(viaDefinition, null, 2));
+      const keyboardDefinition = require(
+        `../public/keyboards/${version}/${filename}`,
+      );
+      const viaDefinition =
+        version === 'v2'
+          ? keyboardDefinitionV2ToVIADefinitionV2(keyboardDefinition)
+          : keyboardDefinitionV3ToVIADefinitionV3(keyboardDefinition);
+      fs.writeFileSync(
+        `./public/definitions/${version}/${filename}`,
+        JSON.stringify(viaDefinition, null, 2),
+      );
       vpids[version].push(viaDefinition.vendorProductId);
     }
   }
 
   const supportedKbs = require('../public/definitions/supported_kbs.json');
   supportedKbs.vendorProductIds = vpids;
-  fs.writeFileSync('./public/definitions/supported_kbs.json', JSON.stringify(supportedKbs, null, 2));
+  fs.writeFileSync(
+    './public/definitions/supported_kbs.json',
+    JSON.stringify(supportedKbs, null, 2),
+  );
 
   const hash = hashJSON(supportedKbs);
 
